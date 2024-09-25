af397da988d3238b4326d1ac883f72d70ccae899
Chris Barker <Chris.Barker@noaa.gov>
2024-09-24 23:04:53 -0400

    Updated for OS-X ARM

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -ex
 
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
 ./configure \
     --prefix="$PREFIX" \
     --disable-silent-rules \
